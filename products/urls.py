21b82a740eee73b5c8271850f5190345ac731b35
Sondosgit <132937887+Sondosgit@users.noreply.github.com>
2023-06-02 15:25:11 +0300

    Delete products directory

--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    
-   path('',views.products,name='products'),
-   path("<int:product_id>", views.product,name='product'),
-   path('search/', views.search ,name='search') ,
-]
